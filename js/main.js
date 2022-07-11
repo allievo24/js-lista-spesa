2333a078278982f9c71cf8cf5e729f4e92b6e1fe
saidmoussadek <saiddus24@gmail.com>
2022-07-11 16:38:51 +0200

    fine

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const listaSpesa=['Pane','Burro','marmellata','Caffè','Latte',];
+                   
+                   
+                   
+                   
+ let prodotto=0;                 
+                  
+while(prodotto < listaSpesa.length){
+      prodotto++   
+            
+      
+}
+    
+console.log(prodotto)      
